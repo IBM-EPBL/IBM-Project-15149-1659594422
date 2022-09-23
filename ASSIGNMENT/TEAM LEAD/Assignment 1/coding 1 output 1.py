4fbe8bf5da4c9b7055d98425ca0b9abbc8c37e73
Ashwiniumeshbabu <113897600+Ashwiniumeshbabu@users.noreply.github.com>
2022-09-23 14:50:38 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM LEAD/Assignment 1/coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Admin\Desktop\ASSIGNMENT\TEAM LEAD\Assignment 1\coding 1.py
+[11, 12, 13, 14, 15]
+[11, 12, 13, 14, 15, 16]
+[12, 13, 14, 15, 16]
+[12, 13, 14, 15, 16, 20]
+[12, 13, 14, 15, 16, 20]
+[12, 13, 14, 15, 16]
+[16, 15, 14, 13, 12]
+PrintWorld
+gnirtS
+tr
