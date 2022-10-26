57581ddfcae1e1af2136d98de208aa0d2db5c2c4
kamalivel <113900925+kamalivel@users.noreply.github.com>
2022-10-26 19:15:40 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM MEMBER 3/Assignment 1/coding 1 output 1.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\Admin\Desktop\ASSIGNMENT\TEAM MEMBER 1\Assignment 1\coding 1.py
+[91, 92, 93, 94, 95]
+[91, 92, 96, 93, 94, 95]
+[92, 96, 93, 94, 95]
+[92, 96, 93, 94, 95, 15]
+[15, 92, 93, 94, 95, 96]
+[15, 92, 93, 94, 95]
+[95, 94, 93, 92, 15]
+Printhello
+gnirtS
+ri
---
+++ ASSIGNMENT/TEAM MEMBER 3/Assignment 1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[91,92,93,94,95]
+print(list)
+
+#insertion
+list.insert(2,96)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(15)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Print")
+str2=("hello")
+print(str1+str2)
+
+#reverse of String
+str3="String"
+print(str3[::-1])
+
+#slice
+print(str3[slice(2,4)])
+
+
+
+
+
+
+
+
+
+
+
+
