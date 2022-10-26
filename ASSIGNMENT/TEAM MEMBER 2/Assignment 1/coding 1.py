19006419ceddb9bd48225d01d5d4d9190b4ee3a1
Mohana Sakthi <100305678+mohanasakthii@users.noreply.github.com>
2022-10-26 19:32:59 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM MEMBER 2/Assignment 1/coding 1.py
@@ -0,0 +1,53 @@
+
+
+list=[51,52,53,54,55]
+print(list)
+
+#insertion
+list.insert(5,56)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(13)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Hello")
+str2=("World")
+print(str1+str2)
+
+#reverse of String
+str3="Hardik"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,17)])
+
+
+
+
+
+
+
+
+
+
+
